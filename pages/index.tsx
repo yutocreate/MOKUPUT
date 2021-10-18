e8908ca16b5e7f2d5af9cc591d6500c98ff23bfe
idenyuto <1otuy26@gmail.com>
2021-10-18 22:08:00 +0900

    Next.jsとFirestoreの連携。データ取得して連携を確認。

---
+++ pages/index.tsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import "firebase/firestore";
+import { db } from "../firebase/firebase";
+
+// console.log(db)
+
+const Home = () => {
+  const [fire, setFire] = useState([]);
+
+  const handleClick = () => {
+    const unSub = db.collection("users").onSnapshot((snapshot) => {
+      setFire(
+        snapshot.docs.map((doc) => ({
+          age: doc.data().age,
+          name: doc.data().name,
+        }))
+      );
+    });
+    return () => unSub();
+  };
+
+  console.log(fire)
+
+  return (
+    <>
+      <h1>あああ</h1>
+      <button onClick={handleClick}>取得</button>
+    </>
+  );
+};
+
+export default Home;
---
+++ pages/signup.tsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react'
+
+const signup = () => {
+// const [email, setEmail] = useState('')
+// const [password, setPassword] = useState('')
+
+  return (
+    <div>
+      <form>
+        {/* <input type='text' value={email} onChange={e => setEmail(e.target.value)}/> */}
+      </form>
+      <h1>a</h1>
+    </div>
+  )
+}
+
+export default signup
