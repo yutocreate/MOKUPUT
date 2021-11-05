9aa1d5d761b36ced4220d829ef5114882f0b9b13
idenyuto <1otuy26@gmail.com>
2021-11-05 12:19:22 +0900

    チャットページのCSSを整えた

--- components/Message.jsx
+++ components/Message.jsx
@@ -1,41 +1,70 @@
 import React, { useRef, useEffect } from "react";
 import Moment from "react-moment";
-import classes from '../styles/Message.module.scss'
+import classes from "../styles/Message.module.scss";
 
 const Message = (props) => {
   const { message, user1 } = props;
   const scrollRef = useRef();
 
   useEffect(() => {
-    scrollRef.current.scrollIntoView({ behaivor: "smooth", block: 'end' });
+    scrollRef.current.scrollIntoView({ behaivor: "smooth", block: "end" });
   }, [message]);
-
 
   return (
     <>
       <div
-        className={`${classes.message_wrapper} ${message && message.from === user1 ? classes.own : ""}`}
+        className={`${classes.message_wrapper} ${
+          message && message.from === user1 ? classes.own : ""
+        }`}
         ref={scrollRef}
       >
         {message.media ? (
-        <p className={`${message.from === user1 ? classes.me : classes.friend}`}>
-          {message.media ? (
-            <img src={message.media} alt={message.text} />
-          ) : null}
-          <span className={classes.span}>{message.text}</span>
-          <br />
-          <small>
-            <Moment fromNow>{message.createdAt.toDate()}</Moment>
-          </small>
-        </p>
+          <>
+            <p
+              className={`${
+                message.from === user1 ? classes.me : classes.friend
+              }`}
+            >
+              {message.media ? (
+                <img src={message.media} alt={message.text} />
+              ) : null}
+              <span className={classes.span}>{message.text}</span>
+              <br />
+            </p>
+            <div
+              className={`${
+                message.from === user1
+                  ? classes.moment_containerI
+                  : classes.moment_containerYou
+              }`}
+            >
+              <small>
+                <Moment fromNow>{message.createdAt.toDate()}</Moment>
+              </small>
+            </div>
+          </>
         ) : (
-        <p className={`${classes.message_wrapper2} ${message.from === user1 ? classes.me : classes.friend}`}>
-          <span className={classes.span}>{message.text}</span>
-          <br />
-          <small>
-            <Moment fromNow>{message.createdAt.toDate()}</Moment>
-          </small>
-        </p>
+          <>
+            <p
+              className={`${classes.message_wrapper2} ${
+                message.from === user1 ? classes.me : classes.friend
+              }`}
+            >
+              <span className={classes.span}>{message.text}</span>
+              <br />
+            </p>
+            <div
+              className={`${
+                message.from === user1
+                  ? classes.moment_containerI
+                  : classes.moment_containerYou
+              }`}
+            >
+              <small>
+                <Moment fromNow>{message.createdAt.toDate()}</Moment>
+              </small>
+            </div>
+          </>
         )}
       </div>
     </>
