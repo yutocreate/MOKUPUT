a43a92272d61d63745c4c762df6de57debb895d5
idenyuto <1otuy26@gmail.com>
2021-11-05 11:37:16 +0900

    チャットページのCSSを少し変更

--- components/Message.jsx
+++ components/Message.jsx
@@ -17,16 +17,26 @@ const Message = (props) => {
         className={`${classes.message_wrapper} ${message && message.from === user1 ? classes.own : ""}`}
         ref={scrollRef}
       >
+        {message.media ? (
         <p className={`${message.from === user1 ? classes.me : classes.friend}`}>
           {message.media ? (
             <img src={message.media} alt={message.text} />
           ) : null}
-          <small className={classes.small}>{message.text}</small>
+          <span className={classes.span}>{message.text}</span>
           <br />
           <small>
             <Moment fromNow>{message.createdAt.toDate()}</Moment>
           </small>
         </p>
+        ) : (
+        <p className={`${classes.message_wrapper2} ${message.from === user1 ? classes.me : classes.friend}`}>
+          <span className={classes.span}>{message.text}</span>
+          <br />
+          <small>
+            <Moment fromNow>{message.createdAt.toDate()}</Moment>
+          </small>
+        </p>
+        )}
       </div>
     </>
   );
